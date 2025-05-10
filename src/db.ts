49b3701a04d9d19e13848bd4cf922e38234daa05
dsalangin <dsalangin74@gmail.com>
2025-05-10 20:43:32 +0300

    feat: add uuid for id

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,9 @@
-export const db = new Map();
-db.set('123', {
-  name: 'Dima',
+export const db = new Map<string, User>();
+db.set('ffc0eaf9-0a05-4ba6-9bda-a83963a49a98', {
+  id: 'ffc0eaf9-0a05-4ba6-9bda-a83963a49a98',
+  username: 'string',
+  age: 123,
+  hobbies: [],
 });
 
 type User = {
--- src/get.ts
+++ src/get.ts
@@ -1,11 +1,23 @@
 import { ServerResponse } from 'http';
 import { sendResponse } from './utils';
+import { validate } from 'uuid';
 import { db } from './db';
 
 export const get = (id: string | undefined, res: ServerResponse) => {
   if (!id) {
     const users = getAllUsers();
     sendResponse(res, 200, JSON.stringify(users));
+    return;
+  }
+
+  const isValid = validate(id);
+
+  if (!isValid) {
+    sendResponse(
+      res,
+      400,
+      JSON.stringify({ message: `User id ${id} is invalid` }),
+    );
     return;
   }
 
