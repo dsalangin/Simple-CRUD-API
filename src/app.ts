c8c0cd37eff92abec068e9a73091f5091691b68c
dsalangin <dsalangin74@gmail.com>
2025-05-10 13:45:47 +0300

    feat: create server and red dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,19 @@
-export const log = () => {
-  console.log(3);
-};
+import { createServer } from 'http';
+import 'dotenv/config';
+
+const { log } = console;
+
+const port = process.env.PORT || 3000;
+
+log(process.env.PORT);
 
 export const init = () => {
-  log();
+  const server = createServer(async (req, res) => {
+    log(req);
+    res.end('Success');
+  });
+
+  server.listen(port, () => {
+    log(`http://localhost:${port}`);
+  });
 };
