ea5f3e19762ed04d2a0c80e910efdd415a557b0a
dsalangin <dsalangin74@gmail.com>
2025-05-11 12:34:18 +0300

    chore: add build app

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const config = {
+  entry: './index.ts',
+  target: 'node',
+  mode: 'production',
+  output: {
+    path: path.resolve(dirname(fileURLToPath(import.meta.url)), 'dist'),
+    filename: 'index.js',
+    libraryTarget: 'module',
+  },
+  experiments: {
+    outputModule: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
+
+export default config;
